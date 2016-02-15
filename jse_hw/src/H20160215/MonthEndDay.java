c3c223c2d724e82c2b68427cdf64b7d83c232f32
user <user@192.168.10.38>
2016-02-15 18:05:06 +0900

    .

---
+++ jse_hw/src/H20160215/MonthEndDay.java
@@ -0,0 +1,15 @@
+package H20160215;
+
+public class MonthEndDay {
+	/*
+	 * 월을 입력하면 해당 월의 끝날이 몇일인지 출력하는 프로그램
+	 * 1을 입력하면 31일
+	 * 단, 2월은 29일로 통일
+	 * 주의점... 입력값이 1~12를 넘어서면 잘못된 입력값임을 공지
+	 * sw.. 로 처리
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
+}
