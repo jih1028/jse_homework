bcff6757e13300a5c06f4194ba5f8b5bb5ac2fda
user <user@192.168.10.38>
2016-02-15 18:16:04 +0900

    .

--- jse_hw/src/H20160215/InputGugudan.java
+++ jse_hw/src/H20160215/InputGugudan.java
@@ -1,5 +1,7 @@
 package H20160215;
 
+import java.util.Scanner;
+
 public class InputGugudan {
 	/*
 	 * 단수를 입력받아서 구구단을 출력함
@@ -7,4 +9,16 @@ public class InputGugudan {
 	 * 단, 입력값이 0 또는 음수일땐 잘못 입력받았다고 하고 프로그램 종료
 	 * for loop으로 처리
 	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("단수를 입력하시오");
+		int s;
+		s = scanner.nextInt();
+		
+		for (int i = 1; i <= 9; i++) {
+			
+			System.out.println(s+"*" + i +"=" + s*i);
+		}
+		
+	}
 }
--- jse_hw/src/H20160215/MonthEndDay.java
+++ jse_hw/src/H20160215/MonthEndDay.java
@@ -1,5 +1,7 @@
 package H20160215;
 
+import java.util.Scanner;
+
 public class MonthEndDay {
 	/*
 	 * 월을 입력하면 해당 월의 끝날이 몇일인지 출력하는 프로그램
@@ -9,7 +11,20 @@ public class MonthEndDay {
 	 * sw.. 로 처리
 	 */
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
-		
+		int month = 0;
+		System.out.println("월을 입력하시오 : ");
+		month = scanner.nextInt();
+		int endOfDay=0;
+		switch (month) {
+		case 1:case 3:case 5:case 7:case 8:case 10:case 12:endOfDay=31;break;
+		case 4:case 6:case 9:case 11:endOfDay=30;break;
+		case 2:endOfDay=29;break;
+		default:System.out.println("잘못된 값을 입력했음"); break;
+		}
+		if(endOfDay != 0){
+			System.out.println("마지막 날은 : "+endOfDay);
+		}
 	}
 }
